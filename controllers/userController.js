2789e860d7e65d6e6304fab54d8f37532d2c925d
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-01-15 18:59:03 -0700

    Added friend routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,49 @@
-const {Thought, User} = require('../models')
+const { Thought, User } = require("../models");
 
 const userController = {
-    getUsers(req, res){User.find()
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
-    },
-    postUser(req, res){User.create(req.body)
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
-    },
-    getUser(req, res){User.findOne({id: req.params.id})
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
-    },
-    deleteUser(req, res){User.findOneAndRemove({id: req.params.id})
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
-    },
-    updateUser(req, res){User.findOneAndUpdate({id: req.params.id}, {$set: req.body})
-    .then(data => res.json(data))
-    .catch(err => res.json(err))
-    }
+  getUsers(req, res) {
+    User.find()
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
+  postUser(req, res) {
+    User.create(req.body)
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
+  getUser(req, res) {
+    User.findOne({ id: req.params.id })
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndRemove({ id: req.params.id })
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate({ id: req.params.id }, { $set: req.body })
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
+  postFriend(req, res) {
+    User.findOneAndUpdate(
+      { id: req.params.id },
+      { $addToSet: { friends: req.params.id } },
+      { new: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { id: req.params.id },
+      { $pull: { friends: req.params.id } },
+      { new: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = userController;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,11 +4,15 @@ const {
     getUser, 
     postUser, 
     updateUser, 
-    deleteUser
+    deleteUser,
+    postFriend,
+    deleteFriend
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(postUser);
 
 router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+
+router.route('/:id/friends/:id').post(postFriend).delete(deleteFriend);
 
 module.exports = router;
